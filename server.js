fe64040877a98f98d35c30993c1af025acdae22d
Anna An <annaan.golf@gmail.com>
2021-10-16 22:13:40 +1300

    updated

--- server.js
+++ server.js
@@ -7,26 +7,28 @@ const articleRouter = require('./routes/articles');
 
 mongoose.connect('mongodb+srv://user123:test987@cluster0.lodca.mongodb.net/markdown-blog?retryWrites=true&w=majority', { useNewURLParser: true, useUnifiedTopology: true })
 
-// deleted middle comment, added new comment
-
+let portNumber = 5000;
 let blog = [];
 Article.find()
   .then(res => {
     blog = res;
-    console.log(res, blog, 'hello')
+    // console.log(res, blog, 'hello')
   })
   .catch(err => {
     console.log(err)
   })
-app.listen(5000);
+app.listen(portNumber);
+console.log(`server started at port ${portNumber}`)
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
 
 app.get('/', async (req, res) => {
-  const articles = await Article.find().sort({ createdAt: -1 });
+  // const articles = await Article.find().sort({ createdAt: -1 });
+  const articles = await Article.find().sort({ likes: -1 });
   res.render('articles/index', { articles });
 })
+
 
 app.use('/articles', articleRouter);
